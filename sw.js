94f7e1e2d45592a3ca40ff7a9875d723b4d50c7f
logta-Die-Bildungswerkstatt <logta@web.de>
2026-08-16 12:04:11 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,13 +1,28 @@
-const CACHE_NAME = 'logta-pro-v1';
+const CACHE_NAME = 'logta-pro-v3';
 const urlsToCache = [
   '/logta-industriemeister/',
   '/logta-industriemeister/index.html'
 ];
 
 self.addEventListener('install', function(event) {
+  self.skipWaiting();
   event.waitUntil(
     caches.open(CACHE_NAME).then(function(cache) {
       return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener('activate', function(event) {
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames
+          .filter(function(name) { return name !== CACHE_NAME; })
+          .map(function(name) { return caches.delete(name); })
+      );
+    }).then(function() {
+      return self.clients.claim();
     })
   );
 });
